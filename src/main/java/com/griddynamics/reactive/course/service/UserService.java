7c2e52f071d22e6e8d607987573a859686ccc8aa
jasiek-stezalski <janstezalski@gmail.com>
2023-02-15 14:28:21 +0100

    Added tests

--- src/main/java/com/griddynamics/reactive/course/service/UserService.java
+++ src/main/java/com/griddynamics/reactive/course/service/UserService.java
@@ -2,12 +2,10 @@ package com.griddynamics.reactive.course.service;
 
 import com.griddynamics.reactive.course.dto.OrderInfo;
 import com.griddynamics.reactive.course.dto.OrderWithProducts;
-import com.griddynamics.reactive.course.entity.Users;
 import com.griddynamics.reactive.course.repository.UserRepository;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 @Service
 public class UserService {
@@ -21,8 +19,10 @@ public class UserService {
 
     @SneakyThrows
     public Flux<OrderInfo> getUserOrdersInfoById(String userId, String requestId) {
-        Users user = userRepository.findBy_id(userId).toFuture().get();
-        Flux<OrderWithProducts> ordersWithProducts = orderService.getOrdersWithProducts(user.getPhone(), requestId);
-        return ordersWithProducts.map(orders -> new OrderInfo(user, orders));
+        return userRepository.findBy_id(userId)
+                .flatMapMany(user -> {
+                    Flux<OrderWithProducts> ordersWithProducts = orderService.getOrdersWithProducts(user.getPhone(), requestId);
+                    return ordersWithProducts.map(orders -> new OrderInfo(user, orders));
+                });
     }
 }
